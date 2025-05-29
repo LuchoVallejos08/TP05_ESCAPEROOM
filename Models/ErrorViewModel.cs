12dc6318b247feaecfaad34dc12f51ba48d8015f
LUCIANO JOAQUIN VALLEJOS <49194424@ort.edu.ar>
2025-05-29 15:08:11 -0300

    cree el escape room

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TP05_EscapeRoomSpiderman.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
