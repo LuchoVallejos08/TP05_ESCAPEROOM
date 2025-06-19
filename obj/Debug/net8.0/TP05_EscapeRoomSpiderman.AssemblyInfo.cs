ce540ed5c224bd267bee4ab684b5aac3bac9eb54
NAOMI FLOR SEREBRINSKY <49627257@ort.edu.ar>
2025-06-19 14:26:20 -0300

    Miedo a perder todo

--- obj/Debug/net8.0/TP05_EscapeRoomSpiderman.AssemblyInfo.cs
+++ obj/Debug/net8.0/TP05_EscapeRoomSpiderman.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP05_EscapeRoomSpiderman")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c4dbdfb9610ba37563fbfa30e036a782d728b5c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4916f5a94cdd5dea6d292de888efcfad905cbfe")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP05_EscapeRoomSpiderman")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP05_EscapeRoomSpiderman")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
